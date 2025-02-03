353636b74a47a853c8f8cb264a93e68c19217711
kaurikallaste <kaurikallaste@gmail.com>
2025-02-03 15:49:02 +0200

    initial commit

---
+++ src/main/java/com/kaurikallaste/jdsa/JdsaApplication.java
@@ -0,0 +1,13 @@
+package com.kaurikallaste.jdsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaurikallaste/jdsa/JdsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaurikallaste.jdsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
